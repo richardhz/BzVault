17431c0ba312a7a558877a6c1ac02aecda121635
Richard Hernandez <richardhz@users.noreply.github.com>
2021-09-21 13:50:26 +0100

    Delete dialog UI

--- Components/DisplayDetail.razor.cs
+++ Components/DisplayDetail.razor.cs
@@ -1,5 +1,6 @@
 ﻿using BzVault.Models;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,33 @@ namespace BzVault.Components
 {
     public class DisplayDetailBase : ComponentBase
     {
+        [Inject] IDialogService DialogService { get; set; }
         [Parameter]
         public ApiLoginData Record { get; set; }
         [Parameter]
         public EventCallback<int> OnProcessDelete { get; set; }
         [Parameter]
         public EventCallback<int> OnProcessEdit { get; set; }
+
+        protected async Task OpenDeleteDialog()
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("ContentText", $"Do you really want to delete {Record.Name} record? This process cannot be undone.");
+            parameters.Add("ButtonText", $"Delete");
+            parameters.Add("Color", Color.Error);
+
+            var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall, DisableBackdropClick = true };
+
+            var dialog = DialogService.Show<Confirmation_Dialog>($"Delete  {Record.Name}", parameters, options);
+
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+                var str = $"Deleting {Record.Name}";
+                Console.WriteLine(str);
+            }
+
+        }
     }
 }
